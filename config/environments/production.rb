a09f1183816faca7e75e2ccad5ec0eed6ee2d956
Yi Zeng <developer@yizeng.me>
2020-11-01 11:26:42 +0100

    Update to generate an API only Rails app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,32 +1,12 @@
-insert_into_file "config/environments/production.rb",
-                 after: /# config\.assets\.css_compressor = :sass\n/ do
-  <<-RUBY
-
-  # Disable minification since it adds a *huge* amount of time to precompile.
-  # Anyway, gzip alone gets us about 70% of the benefits of minify+gzip.
-  config.assets.css_compressor = false
-  RUBY
-end
-
 uncomment_lines "config/environments/production.rb", /config\.force_ssl = true/
 gsub_file "config/environments/production.rb",
           "config.force_ssl = true",
           'config.force_ssl = ENV["RAILS_FORCE_SSL"].present?'
+gsub_file "config/environments/production.rb",
+          "config.log_level = :debug",
+          'config.log_level = :info'
 
-insert_into_file "config/environments/production.rb",
-                 after: /# config\.action_mailer\.raise_deliv.*\n/ do
-  <<-RUBY
-
-  # Production email config
-  config.action_mailer.delivery_method = :postmark
-  config.action_mailer.postmark_settings = {
-    api_token: ENV.fetch("POSTMARK_API_KEY")
-  }
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = {
-    host: "#{production_hostname}",
-    protocol: "https"
-  }
-  config.action_mailer.asset_host = "https://#{production_hostname}"
-  RUBY
-end
+uncomment_lines "config/environments/production.rb", /config\.cache_store = :mem_cache_store/
+gsub_file "config/environments/production.rb",
+          "config.cache_store = :mem_cache_store",
+          'config.cache_store = :redis_cache_store, { url: ENV["REDIS_URL_CACHING"] } if ENV["REDIS_URL_CACHING"].present?'
--- config/initializers/generators.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.config.generators do |g|
-  # Disable generators we don't need.
-  g.javascripts false
-  g.stylesheets false
-end
